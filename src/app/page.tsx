82649cb275513a06ba87d36b30e1f900a8801673
Aleksandr Marchenko <proshta@gmail.com>
2024-12-02 16:59:55 +0200

    02.12.2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <div>
-          <h1>
+    <div className="bg-black bg-home-img bg-cover bg-center">
+      <main
+      className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh"
+      >
+        <div
+        className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl"
+        >
+          <h1
+          className="text-3xl font-bold sm:text-4xl"
+          >
             Platen&apos;s Computer <br /> Repair Shop
           </h1>
           <address>
