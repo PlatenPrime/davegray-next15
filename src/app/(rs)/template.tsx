8e4b5246b4ce14f8fd0e1735c2ef173b4cdf49a3
Aleksandr Marchenko <proshta@gmail.com>
2024-12-05 14:49:23 +0200

    iutrf6

--- src/app/(rs)/template.tsx
+++ src/app/(rs)/template.tsx
@@ -1,12 +1,11 @@
-export default async function RSLayout({
+export default async function Template({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <div className="mx-auto w-full max-w-7xl">
-        {/* Header */}
-        <div className="px-4 py-2"> {children} </div>
+      <div className="animate-appear">
+       {children} 
       </div>
     );
   }
